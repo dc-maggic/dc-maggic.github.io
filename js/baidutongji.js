4e3a1170e6238b5440b371849d3c1971b5148b1a
dc-maggic <dc-maggic@outlook.com>
2018-10-09 11:32:06 +0800

    Site updated: 2018-10-09 11:32:05

---
+++ js/baidutongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2a57f2bd18d5a660ff1196e713cc2783";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
